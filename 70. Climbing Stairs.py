5e3acd50d1a8ee256adbef868cbeca6da48eba9f
PRABHJOT SINGH <104373186+SARDARJI222@users.noreply.github.com>
2023-02-18 11:14:40 +0530

    Update 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,7 +1,25 @@
-class Solution:
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n ==1:
+#             return 1
+#         if n== 2:
+#             return 2
+#         return self.climbStairs(n-2) +self.climbStairs(n-1)
+# here time limit exceed 
+    
+    
+    
+    class Solution:
     def climbStairs(self, n: int) -> int:
         if n ==1:
             return 1
         if n== 2:
             return 2
-        return self.climbStairs(n-2) +self.climbStairs(n-1)
+        # return self.climbStairs(n-2) +self.climbStairs(n-1)
+        two_back=2
+        one_back=1
+        for i in range(3,n+1):
+            ret = two_back + one_back
+            two_back=one_back
+            one_back=ret
+        return ret
